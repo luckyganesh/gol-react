59413e0e7e9e0b0976c234c691d7e2f8bd62ebe7
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2019-04-02 16:47:19 +0530

    Updates

---
+++ precache-manifest.ccc80e0e6d2f906245fbb7b7dddb6be6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b3a64ffaf3dbbc41a20",
+    "url": "/gol-react/static/css/main.661ed29c.chunk.css"
+  },
+  {
+    "revision": "9b3a64ffaf3dbbc41a20",
+    "url": "/gol-react/static/js/main.507c9ebd.chunk.js"
+  },
+  {
+    "revision": "d99d7a9e223364735716",
+    "url": "/gol-react/static/js/runtime~main.b14f0b41.js"
+  },
+  {
+    "revision": "511a6eb6b7ec2e1e4ec3",
+    "url": "/gol-react/static/js/2.26ba37d3.chunk.js"
+  },
+  {
+    "revision": "be9e075b99dbcd565ff1b19311ef7982",
+    "url": "/gol-react/index.html"
+  }
+];
